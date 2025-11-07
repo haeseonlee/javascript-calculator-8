8395e0aa3629b30789196f97b00d8c8ac479dc6b
haeseon lee <haeseon4816@gmail.com>
2025-11-07 20:41:09 +0900

    feat: 프로그램 실행 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import Calculator from "./Calculator.js";
 
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+
+    const calculator = new Calculator();
+
+    const result = calculator.add(input);
+
+    MissionUtils.Console.print(`결과 : ${result}`);
   }
 }
 
